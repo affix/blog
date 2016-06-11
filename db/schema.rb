91af214d86f1316bd04aa95abcf2bec627a57f53
Keiran Smith <affix@affix.me>
2016-06-12 00:07:26 +0100

    Add Tag Support

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610212852) do
+ActiveRecord::Schema.define(version: 20160611230553) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
