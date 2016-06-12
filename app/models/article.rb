093e08c203ac1b80676bd47dc469a7c7082b3b02
Keiran Smith <affix@affix.me>
2016-06-12 18:00:46 +0100

    SEO Friendly URLs

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,8 @@ class Article < ActiveRecord::Base
   has_and_belongs_to_many :tag
   belongs_to :user
   accepts_nested_attributes_for :tag, :allow_destroy => false
+
+  def to_param
+    "#{id}-#{title.downcase.gsub(/[^a-zA-Z0-9]+/, '-').gsub(/-{2,}/, '-').gsub(/^-|-$/, '')}"
+  end
 end
