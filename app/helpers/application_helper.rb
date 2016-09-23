bd37201c32f84e3bb7d58cc1db1c5a5337d17378
Keiran Smith <affix@affix.me>
2016-09-23 13:26:57 +0100

    Update Style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
   def get_tag_list
     Tag.all
   end
+
+  def embedded_svg filename, options={}
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:id].present?
+      svg['id'] = options[:id]
+    end
+    doc.to_html.html_safe
+  end
 end
